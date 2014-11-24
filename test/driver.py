1810d5b37d109c6d3187f426e78017f8d9d187f5
Ryan Marquardt <ryan.marquardt@gmail.com>
2014-11-24 12:19:44 -0800

    Add create_table testing

--- test/driver.py
+++ test/driver.py
@@ -5,5 +5,17 @@ import dibi
 import logging
 
 
-def test_driver(driver, parameters):
-    db = dibi.DB(driver(**parameters))
+class test_driver(object):
+    def __init__(self, driver, parameters):
+        self.db = dibi.DB(driver(**parameters))
+        self.create_table()
+
+    def create_table(self):
+        table_1 = self.db.add_table('table 1')
+        table_1.add_column('name', dibi.datatype.Text)
+        table_1.add_column('number', dibi.datatype.Integer)
+        table_1.add_column('value', dibi.datatype.Float)
+        table_1.add_column('binary_data', dibi.datatype.Blob)
+        table_1.add_column('timestamp', dibi.datatype.DateTime)
+        table_1.save()
+
