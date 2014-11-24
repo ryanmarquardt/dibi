e733f799fca707a62136daa39ae6d415f0e0721c
Ryan Marquardt <ryan.marquardt@gmail.com>
2014-11-24 15:25:38 -0800

    Log more information from assertion failures

--- test/driver.py
+++ test/driver.py
@@ -5,6 +5,8 @@ import dibi
 import datetime
 import logging
 
+import inspect
+
 
 class catch_failure(object):
     def __init__(self, test):
@@ -17,6 +19,26 @@ class catch_failure(object):
         if exc is not None:
             self.test.logger.info(self.test.db.driver.last_statement)
             self.test.logger.info(self.test.db.driver.last_values)
+        if exc is AssertionError:
+            while tb.tb_next:
+                tb = tb.tb_next
+            frame = tb.tb_frame
+            code = frame.f_code
+            source, first_line = inspect.getsourcelines(tb)
+            source = source[frame.f_lineno - first_line]
+            local_vars = {key: value for key, value in frame.f_locals.items()
+                          if key in source}
+            fail_text = ("Failure in {module}.{function}:{line}\n{source}  # "
+                         "{locals}")
+            self.test.logger.error(fail_text.format(
+                module=inspect.getmoduleinfo(code.co_filename).name,
+                function=code.co_name,
+                line=frame.f_lineno,
+                source=source.strip(),
+                locals=", ".join("{}={!r}".format(*item) for item in
+                                 local_vars.items()),
+            ))
+            return True
 
 
 class test_driver(object):
@@ -38,7 +60,7 @@ class test_driver(object):
 
     def list_tables(self):
         tables = list(self.db.driver.list_tables())
-        assert tables == ['table 1'], tables
+        assert tables == ['table 1']
 
     def insert_rows(self):
         sample_1_id = self.db.tables['table 1'].insert(
@@ -48,4 +70,4 @@ class test_driver(object):
             binary_data=b'\xa8\xe2u\xf5pZ\x1c\x82R5\x01\xe7UC\x06',
             timestamp=datetime.datetime(1900, 1, 1, 12, 15, 14),
         )
-        assert sample_1_id == 1, sample_1_id
+        assert sample_1_id == 1
